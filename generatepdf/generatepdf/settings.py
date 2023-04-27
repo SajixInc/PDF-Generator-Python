9cd1e0b0efdacb6a5542288bec36b105669d743a
sacredabhishek <3474686+sacredabhishek@users.noreply.github.com>
2023-04-27 08:51:35 +0000

    changes for linux

--- generatepdf/generatepdf/settings.py
+++ generatepdf/generatepdf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-pdjdkp_&e$kjgchw3*4p61&@=9e)4k=*p8a_zb39flj@it2x+y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
